35e0b93471c3743565588d7ddb602ae3b8cdf3f3
S1206332 <S1206332@student.windesheim.nl>
2023-11-30 22:01:06 +0100

    paymenturl

--- site/payment.php
+++ site/payment.php
@@ -13,6 +13,52 @@ if(getCartPrice()>100) {
 }
 $totalValue = number_format((float)getCartTotalPrice($verzendkosten), 2, ".", "");
 
+$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+// Zoek de positie van de laatste /
+$lastSlashPos = strrpos($url, '/');
+
+// Controleer of er daadwerkelijk een / in de string zit
+if ($lastSlashPos !== false) {
+    // Haal de substring op tot de laatste / (niet inclusief)
+    $trimmedUrl = substr($url, 0, $lastSlashPos);
+} else {
+    // Geen / gevonden, dus de originele string blijft onveranderd
+    $trimmedUrl = $url;
+}
+
+
+
+//https://github.com/mollie/mollie-api-php
+
+// $order = $mollie->orders->create([
+//     "amount" => [
+//         "value" => "1027.99",
+//         "currency" => "EUR",
+//     ],
+//     "orderNumber" => "1234",
+//     "lines" => [
+//         [
+//             "name" => "LEGO 42083 Bugatti Chiron",
+//             "quantity" => 1,
+//             "vatRate" => "21.00",
+//             "unitPrice" => [
+//                 "currency" => "EUR",
+//                 "value" => $totalValue,
+//             ],
+//             "totalAmount" => [
+//                 "currency" => "EUR",
+//                 "value" => $totalValue,
+//             ],
+//             "vatAmount" => [
+//                 "currency" => "EUR",
+//                 "value" => "0.00",
+//             ],
+//         ],
+//         // more order line items
+//     ],
+// ]);
+
 
 $method = $mollie->methods->get(\Mollie\Api\Types\PaymentMethod::IDEAL, ["include" => "issuers"]);
 
@@ -22,8 +68,8 @@ $payment = $mollie->payments->create([
         "value" => $totalValue
     ],
     "description" => "My first API payment",
-    "redirectUrl" => "https://webshop.example.org/order/12345/",
-    "webhookUrl"  => "https://webshop.example.org/mollie-webhook/",
+    "redirectUrl" => $trimmedUrl . "/paymentstatus.php?order=1234",
+    "cancelUrl" => $trimmedUrl . "/paymentstatus.php?order=1235",
     "method"      => \Mollie\Api\Types\PaymentMethod::IDEAL,
     "issuer"      => $selectedIssuerId, // e.g. "ideal_INGBNL2A"
 ]);
---
+++ site/paymentstatus.php
@@ -0,0 +1,22 @@
+<?php
+include __DIR__ . "/header.php";
+include __DIR__ . "/cartfuncties.php";
+
+if (isset($_GET['order'])) {
+    $order = $_GET['order'];
+} else {
+    $order = "";
+}
+
+if($order==1234){
+    ?> 
+    <h1>Uw bestelling is voltooid.</h1>
+    <?php
+}elseif($order==1235){
+    ?> 
+    <h1>Uw bestelling is mislukt.</h1>
+    <?php
+}
+
+
+include __DIR__ . "/footer.php";
