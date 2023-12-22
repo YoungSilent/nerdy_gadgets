b302f2de8d42f907268c53b1bee7e202c9bbd967
S1206332 <S1206332@student.windesheim.nl>
2023-12-22 13:02:06 +0100

    verzendkosten

--- site/paymenttest.php
+++ site/paymenttest.php
@@ -2,5 +2,5 @@
 include __DIR__ . "/header.php";
 include __DIR__ . "/cartfuncties.php";
 include __DIR__ . "/paymentfuncties.php";
-print_r(getcart());
-createOrderLines(73603);
+print(getVerzendkosten());
+;
