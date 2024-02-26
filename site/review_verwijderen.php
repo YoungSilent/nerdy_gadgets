91fb7b8ba0fb0f4b8573f78306ef7a051c830334
YoungSilent <71005200+YoungSilent@users.noreply.github.com>
2024-02-26 21:55:45 +0100

    Update review_verwijderen.php

--- site/review_verwijderen.php
+++ site/review_verwijderen.php
@@ -2,18 +2,15 @@
 require_once 'database.php';
 session_start();
 if (!isset($_SESSION['PersonID'])) {
-    // Redirect the user to the login page or display an error message
-    var_dump($_POST['review_id']);
-    var_dump($_SESSION['PersonID']);
+//    var_dump($_POST['review_id']);
+//    var_dump($_SESSION['PersonID']);
+    exit;
 
-//    exit; // Stop further execution of the script
 }
 $connection = connectToDatabase();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get review ID from the form submission
     $reviewID = $_POST['review_id'];
-    $userID = $_SESSION['PersonID']; // Get the user ID from the session
-    // Perform the deletion of the review from the database
+    $userID = $_SESSION['PersonID'];
     $query_delete = "DELETE FROM reviews WHERE id = $reviewID AND PersonID = $userID";
     $result_delete = mysqli_query($connection, $query_delete);
 }
